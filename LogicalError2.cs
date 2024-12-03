0394ba05cede5bafc4f1d9d3511ba83d8eb2e401
Max <113231278+MaxMalamo@users.noreply.github.com>
2024-12-03 10:37:07 +0200

    Add files via upload

---
+++ LogicalError2.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalError2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 50;
+
+            if (age >= 18 && age < 30)
+            {
+                Console.WriteLine("Over 18");
+            }
+            
+            if (age >= 30 && age < 50)
+            {
+                Console.WriteLine("Over 30");
+            }
+            
+            if (age >= 50)
+            {
+                Console.WriteLine("Over 50");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
